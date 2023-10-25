1fa69dcbc669bd3737d9ed99f378db347eb0ceac
Will Boswell <59512235+doujones83@users.noreply.github.com>
2023-10-25 17:25:33 -0400

    Update config.js

--- AWS-Services-App/js/config.js
+++ AWS-Services-App/js/config.js
@@ -1,10 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_yTWPSNLZh',
+        userPoolClientId: '5dei2k7eu1mvllblsfngltmgeg', 
+        region: 'us-east-2' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+        invokeUrl: 'https://4dzgit3g15.execute-api.us-east-2.amazonaws.com/Production' 
 };
